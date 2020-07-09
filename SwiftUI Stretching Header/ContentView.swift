268802601ab71134b194997213254dd1b592f6f6
BAChapin <brett.chapin89@gmail.com>
2020-07-08 23:47:48 -0400

    Added an asset, and created the StretchingHeader view: + Wide Landscape photo asset + StretchingHeader    *Notes* Learned how to use a ViewBuilder property wrapper in a view's initialization method. 	        Created a few helper methods to calculate the view's offset and height adjustment based upon the movement of a ScrollView. ~ ContentView    ~ body was updated to contain the new StretchingHeader view, and a list below it simply listing 20 rows of text

--- SwiftUI Stretching Header/ContentView.swift
+++ SwiftUI Stretching Header/ContentView.swift
@@ -7,9 +7,68 @@
 
 import SwiftUI
 
+struct StretchingHeader<Content: View> : View {
+    let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack(content: content)
+                .frame(width: geo.size.width, height: height(for: geo))
+                .offset(y: offset(for: geo))
+        }
+    }
+    
+    func height(for proxy: GeometryProxy) -> CGFloat {
+        let y = proxy.frame(in: .global).minY
+        return proxy.size.height + max(0, y)
+    }
+    
+    func offset(for proxy: GeometryProxy) -> CGFloat {
+        let y = proxy.frame(in: .global).minY
+        return min(0, -y)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+//        ScrollView {
+//            VStack {
+//                StretchingHeader {
+//                    Image("Wide Landscape")
+//                        .resizable()
+//                        .scaledToFill()
+//                }
+//                .frame(height: 200)
+//            }
+//
+//            Text("Hello World!")
+//        }
+//        .edgesIgnoringSafeArea(.all)
+        
+        List {
+            Section(header:
+                        StretchingHeader {
+                            Image("Wide Landscape")
+                                .resizable()
+                                .scaledToFill()
+                            
+                            Text("Stretching Header Tutorial")
+                                .font(.headline)
+                            Text("From Paul Hudson")
+                                .font(.caption)
+                        }
+                        .frame(height: 200)
+            ) {
+                ForEach(0..<20) { i in
+                    Text("Row \(i + 1)")
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
     }
 }
 
