d1630fc3cf62b8f54f40314e55f921da3d9202b8
BAChapin <brett.chapin89@gmail.com>
2020-07-08 22:55:34 -0400

    Initial Commit

---
+++ SwiftUI Stretching Header/SwiftUI_Stretching_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stretching_HeaderApp.swift
+//  SwiftUI Stretching Header
+//
+//  Created by Brett Chapin on 7/8/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stretching_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
